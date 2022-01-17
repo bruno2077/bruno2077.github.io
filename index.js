091207d8cbb4302212de7fabc4257af58d7fb765
Bruno Borges Gontijo <82050416+bruno2077@users.noreply.github.com>
2022-01-17 19:51:28 -0300

    update infos & some styles

---
+++ index.js
@@ -0,0 +1,13 @@
+// Esse código altera os ícones do font-awesome de tempos em tempos.
+
+const element = document.getElementById("iconsCarrousel");
+const fontIcons = ["fas fa-heartbeat", 'fas fa-music', "far fa-thumbs-up", 'fab fa-css3-alt', 'fas fa-forward', 'fab fa-html5', 
+                    'fas fa-heart-broken', "far fa-hand-peace", 'fas fa-fire-alt', "far fa-grin", "fab fa-js-square" ]
+let index = 0
+setInterval(function(){            
+    element.className = fontIcons[index] + " px-1"
+    ++index
+    if(fontIcons.length <= index)
+        index = 0     
+
+    }, 1000)
